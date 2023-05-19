3a3a51d32b079d0e5561ec89abe5bf747b3aa749
Nam junseong <danielnam0801@gmail.com>
2023-05-19 13:39:19 +0900

    내적사용

--- Assets/01.Scripts/Junseong/ApheliosAttack.cs
+++ Assets/01.Scripts/Junseong/ApheliosAttack.cs
@@ -15,7 +15,8 @@ public class ApheliosAttack : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.Mouse0))
         {
-            Vector3 mousePos = Camera.main.WorldToScreenPoint(Input.mousePosition);
+            Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            mousePos.z = 0;
             Vector3 dir = mousePos - transform.position;
             Bullet spawnBul = Instantiate(bullet, transform.position, Quaternion.identity);
             spawnBul.Shoot(dir);
--- Assets/01.Scripts/Junseong/Bullet.cs
+++ Assets/01.Scripts/Junseong/Bullet.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,15 +6,49 @@ using UnityEngine;
 public class Bullet : MonoBehaviour
 {
     [SerializeField] private float m_Speed = 5f;
+    [SerializeField] private float radius = 3f;
     [SerializeField] Rigidbody rb;
+    [SerializeField] float attackAngleRange = 120f;
+    Vector3 inputDir;
 
     private void Awake()
     {
         rb = GetComponent<Rigidbody>();
     }
 
-    public void Shoot(Vector3 dir)
+    public void Shoot(Vector2 dir)
     {
-        rb.velocity = dir.normalized * m_Speed;
+        inputDir = dir.normalized;
+        rb.velocity = inputDir * m_Speed;
     }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.collider.CompareTag("Enemy"))
+        {
+            SeperateBullet();
+            CheckOtherEnemy(inputDir, collision.contacts[0].point);
+        }
+
+    }
+
+    private void CheckOtherEnemy(Vector3 dir, Vector3 hitPoint) //플레이어가 쏜 총알 방향 벡터
+    {
+        Collider[] enemys = Physics.OverlapSphere(hitPoint, radius, 1 << LayerMask.NameToLayer("Enemy"));
+        for (int i = 0; i < enemys.Length; i++)
+        {
+            Vector2 myDir = (enemys[i].transform.position - hitPoint).normalized; // 맞은 곳에서 범위 안에 있는 적을 향한 벡터
+            float myAngle = Mathf.Acos(Vector3.Dot(inputDir, myDir)) * Mathf.Rad2Deg;// 각도를 구한후 ACOs에 넣음       
+            if (myAngle < attackAngleRange) ;//맞은거
+            else; //안맞은거
+        }
+    }
+
+    private void SeperateBullet()
+    {
+            
+    }
+
+
+
 }
